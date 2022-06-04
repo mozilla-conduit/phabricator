97429edde38170db309095d6acf519f815b8c6d9
Tooru Fujisawa <arai_a@mac.com>
2022-06-05 00:18:21 +0900

    Bug 1467623 - Support bug NNN comment #NNN link in the comment.

--- src/infrastructure/markup/PhabricatorMarkupEngine.php
+++ src/infrastructure/markup/PhabricatorMarkupEngine.php
@@ -517,6 +517,8 @@ final class PhabricatorMarkupEngine extends Phobject {
       $rules[] = new PhabricatorYoutubeRemarkupRule();
     }
 
+    $rules[] = new PhabricatorBugzillaLinkRule();
+
     $rules[] = new PhabricatorIconRemarkupRule();
     $rules[] = new PhabricatorEmojiRemarkupRule();
     $rules[] = new PhabricatorHandleRemarkupRule();
