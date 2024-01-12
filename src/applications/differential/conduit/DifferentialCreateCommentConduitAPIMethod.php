a9d3b03135409338b24ddd3c18fd21773dee54e9
Suhaib Mujahid <smujahid@mozilla.com>
2024-01-12 15:52:30 -0500

    Restore the `loadUnsubmittedInlineComments` function

--- src/applications/differential/conduit/DifferentialCreateCommentConduitAPIMethod.php
+++ src/applications/differential/conduit/DifferentialCreateCommentConduitAPIMethod.php
@@ -96,12 +96,9 @@ final class DifferentialCreateCommentConduitAPIMethod
 
     if ($request->getValue('attach_inlines')) {
       $type_inline = DifferentialTransaction::TYPE_INLINE;
-      $inlines = id(new DifferentialDiffInlineCommentQuery())
-        ->setViewer($viewer)
-        ->withRevisionPHIDs(array($revision->getPHID()))
-        ->withPublishableComments(true)
-        ->execute();
-      $inlines = mpull($inlines, 'newInlineCommentObject');
+      $inlines = $this->loadUnsubmittedInlineComments(
+        $viewer,
+        $revision);
       foreach ($inlines as $inline) {
         $xactions[] = id(new DifferentialTransaction())
           ->setTransactionType($type_inline)
@@ -124,6 +121,51 @@ final class DifferentialCreateCommentConduitAPIMethod
       'revisionid'  => $revision->getID(),
       'uri'         => PhabricatorEnv::getURI('/D'.$revision->getID()),
     );
+  }
+
+  public static function loadUnsubmittedInlineComments(
+    PhabricatorUser $viewer,
+    DifferentialRevision $revision) {
+
+    $inlines = id(new DifferentialDiffInlineCommentQuery())
+      ->setViewer($viewer)
+      ->withRevisionPHIDs(array($revision->getPHID()))
+      ->withAuthorPHIDs(array($viewer->getPHID()))
+      ->withHasTransaction(false)
+      ->withIsDeleted(false)
+      ->needReplyToComments(true)
+      ->execute();
+
+    foreach ($inlines as $key => $inline) {
+      $inlines[$key] = DifferentialInlineComment::newFromModernComment(
+        $inline);
+    }
+
+    PhabricatorInlineComment::loadAndAttachVersionedDrafts(
+      $viewer,
+      $inlines);
+
+    // Don't count void inlines when considering draft state.
+    foreach ($inlines as $key => $inline) {
+      if ($inline->isVoidComment($viewer)) {
+        unset($inlines[$key]);
+        continue;
+      }
+
+      // For other inlines: if they have a nonempty draft state, set their
+      // content to the draft state content. We want to submit the comment
+      // as it is currently shown to the user, not as it was stored the last
+      // time they clicked "Save".
+
+      $draft_content = $inline->getContentForEdit($viewer);
+      if (strlen($draft_content)) {
+        $inline->setContent($draft_content);
+      }
+    }
+
+    $inlines = mpull($inlines, 'getStorageObject');
+
+    return $inlines;
   }
 
 }
