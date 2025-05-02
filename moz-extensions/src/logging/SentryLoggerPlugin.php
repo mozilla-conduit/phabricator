b3587a1a968bd5adefa7da545bc87f89ce120712
David Lawrence <dkl@mozilla.com>
2025-05-02 10:59:16 -0400

    Bug 1963709 - Sentry errors not populating for Phabricator (prod) (improvements)

--- moz-extensions/src/logging/SentryLoggerPlugin.php
+++ moz-extensions/src/logging/SentryLoggerPlugin.php
@@ -52,8 +52,8 @@ class SentryLoggerPlugin extends Phobject {
     return implode('&', $params);
   }
 
-  public static function filter_values($array, $fields_re = null) {
-    if ($fields_re === null) {
+  public static function filter_values($array, $fields_re) {
+    if (empty($fields_re)) {
       $fields_re = '/^(__csrf__|token)$/i';
     }
 
@@ -94,34 +94,12 @@ class SentryLoggerPlugin extends Phobject {
       $request = $event->getRequest();
 
       // Sanitize HTTP POST data
-      $fields_re = '/^(__csrf__|token)$/i';
-      $sanitize = function(&$item, $key, $fields_re) {
-        if (empty($key)) {
-          return;
-        }
-        if (preg_match($fields_re, $key)) {
-          $item = '********';
-        }
-      };
-
-      if (is_array($request['data'])) {
-        array_walk_recursive($request['data'], $sanitize, $fields_re);
-      } else {
-        if (preg_match($fields_re, $request['data'])) {
-          $request['data'] = '********';
-        }
-      }
+      $request['data'] = self::filter_values($request['data']);
 
       if (is_array($request['data']) && array_key_exists('params', $request['data'])) {
         try {
           $params = phutil_json_decode($request['data']['params']);
-          if (is_array($params)) {
-            array_walk_recursive($params, $sanitize, $fields_re);
-          } else {
-            if (preg_match($fields_re, $params)) {
-              $params = '********';
-            }
-          }
+          $params = self::filter_values($params);
           $request['data']['params'] = phutil_json_encode($params);
         } catch (PhutilJSONParserException $ex) {
           // data['params'] wasn't JSON, so we're finished sanitizing the request data.
@@ -130,13 +108,7 @@ class SentryLoggerPlugin extends Phobject {
 
       // Sanitize query string
       $query_data = self::parse_query_str($request['query_string']);
-      if (is_array($query_data)) {
-        array_walk_recursive($query_data, $sanitize, $fields_re);
-      } else {
-        if (preg_match($fields_re, $query_data)) {
-          $query_data = '********';
-        }
-      }
+      $query_data = self::filter_values($query_data);
       $request['query_string'] = self::generate_query_str($query_data);
 
       // Sanitize cookie data
@@ -145,14 +117,7 @@ class SentryLoggerPlugin extends Phobject {
       }
 
       // Sanitize header data
-      $headers_re = '/^(Cookie|X-Phabricator-Csfr)$/i';
-      if (is_array($request['headers'])) {
-        array_walk_recursive($request['headers'], $sanitize, $fields_re);
-      } else {
-        if (preg_match($fields_re, $request['headers'])) {
-          $request['headers'] = '********';
-        }
-      }
+      $request['headers'] = self::filter_values($request['headers'], '/^(Cookie|X-Phabricator-Csfr)$/i');
 
       $event->setRequest($request);
       return $event;
