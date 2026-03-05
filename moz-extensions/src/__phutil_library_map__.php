de69f0709aa1dfefec0e935029daf33d7bd684a4
David Lawrence <dkl@mozilla.com>
2026-03-04 23:25:54 -0500

    Bug 1907593 - Consider changing default setting when a reviewer 'requests changes' so revisions don't disappear from other reviewers' queues

--- moz-extensions/src/__phutil_library_map__.php
+++ moz-extensions/src/__phutil_library_map__.php
@@ -138,6 +138,7 @@ phutil_register_library_map(array(
     'StoryQueryResult' => 'email/adapter/StoryQueryResult.php',
     'TransactionList' => 'email/adapter/TransactionList.php',
     'UserPhabricatorReviewer' => 'email/adapter/UserPhabricatorReviewer.php',
+    'DifferentialRevisionRequiredActionResultBucketSpecial' => 'differential/query/DifferentialRevisionRequiredActionResultBucketSpecial.php',
   ),
   'function' => array(
     'isRevisionPrivate' => 'differential/view/DifferentialRevisionWarning.php',
@@ -230,5 +231,6 @@ phutil_register_library_map(array(
     'SentryConfigOptions' => 'PhabricatorApplicationConfigOptions',
     'SentryLoggerPlugin' => 'Phobject',
     'UserPhabricatorReviewer' => 'PhabricatorReviewer',
+    'DifferentialRevisionRequiredActionResultBucketSpecial' => 'DifferentialRevisionRequiredActionResultBucket'
   ),
 ));
