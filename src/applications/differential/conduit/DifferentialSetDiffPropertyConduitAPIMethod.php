816219584cea78bfaa89f8ed6e1b0b3defd2e386
David Lawrence <dkl@mozilla.com>
2026-05-12 11:07:35 -0400

    Bug 2036370

--- src/applications/differential/conduit/DifferentialSetDiffPropertyConduitAPIMethod.php
+++ src/applications/differential/conduit/DifferentialSetDiffPropertyConduitAPIMethod.php
@@ -25,14 +25,50 @@ final class DifferentialSetDiffPropertyConduitAPIMethod
 
   protected function defineErrorTypes() {
     return array(
-      'ERR_NOT_FOUND' => pht('Diff was not found.'),
+      'ERR_NOT_FOUND'    => pht('Diff was not found.'),
+      'ERR_PERMISSIONS'  => pht('You do not have permission to modify this diff.'),
+      'ERR_INVALID_DATA' => pht('Property data is not valid JSON.'),
     );
   }
 
   protected function execute(ConduitAPIRequest $request) {
+    $viewer = $request->getUser();
     $diff_id = $request->getValue('diff_id');
     $name = $request->getValue('name');
-    $data = json_decode($request->getValue('data'), true);
+
+    try {
+      $data = phutil_json_decode($request->getValue('data'));
+    } catch (PhutilJSONParserException $ex) {
+      throw new ConduitException('ERR_INVALID_DATA');
+    }
+
+    $diff = id(new DifferentialDiffQuery())
+      ->setViewer($viewer)
+      ->withIDs(array($diff_id))
+      ->executeOne();
+    if (!$diff) {
+      throw new ConduitException('ERR_NOT_FOUND');
+    }
+
+    $revision_id = $diff->getRevisionID();
+    if ($revision_id) {
+      $revision = id(new DifferentialRevisionQuery())
+        ->setViewer($viewer)
+        ->withIDs(array($revision_id))
+        ->requireCapabilities(
+          array(
+            PhabricatorPolicyCapability::CAN_VIEW,
+            PhabricatorPolicyCapability::CAN_EDIT,
+          ))
+        ->executeOne();
+      if (!$revision) {
+        throw new ConduitException('ERR_PERMISSIONS');
+      }
+    } else {
+      if ($diff->getAuthorPHID() !== $viewer->getPHID()) {
+        throw new ConduitException('ERR_PERMISSIONS');
+      }
+    }
 
     self::updateDiffProperty($diff_id, $name, $data);
   }
