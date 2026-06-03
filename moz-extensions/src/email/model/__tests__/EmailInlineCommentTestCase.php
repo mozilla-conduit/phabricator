289bcdd51c0cba4c02c80d666a4e456d89733b08
Paul Adenot <paul@paul.cx>
2026-06-03 19:15:37 +0200

    Bug 2044749 - Expose code suggestions in feed.for_email.query inline comments
    Add hasSuggestion and suggestionText fields to EmailInlineComment,
    populated from the inline comment content state. Also adds unit tests
    for the new fields.

---
+++ moz-extensions/src/email/model/__tests__/EmailInlineCommentTestCase.php
@@ -0,0 +1,72 @@
+<?php
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+final class EmailInlineCommentTestCase extends PhabricatorTestCase {
+
+  private function newMessage(): EmailCommentMessage {
+    return new EmailCommentMessage('text', '<p>text</p>');
+  }
+
+  private function newCodeContext(): EmailCodeContext {
+    return new EmailCodeContext([]);
+  }
+
+  public function testDefaultsToNoSuggestion() {
+    $comment = new EmailInlineComment(
+      '/src/foo.php:10',
+      'https://example.com/D1',
+      $this->newMessage(),
+      'code',
+      $this->newCodeContext()
+    );
+
+    $this->assertFalse($comment->hasSuggestion);
+    $this->assertEqual('', $comment->suggestionText);
+  }
+
+  public function testStoresSuggestion() {
+    $comment = new EmailInlineComment(
+      '/src/foo.php:10',
+      'https://example.com/D1',
+      $this->newMessage(),
+      'code',
+      $this->newCodeContext(),
+      true,
+      'return $x + 1;'
+    );
+
+    $this->assertTrue($comment->hasSuggestion);
+    $this->assertEqual('return $x + 1;', $comment->suggestionText);
+  }
+
+  public function testNoSuggestionWithExplicitFalse() {
+    $comment = new EmailInlineComment(
+      '/src/foo.php:10',
+      'https://example.com/D1',
+      $this->newMessage(),
+      'code',
+      $this->newCodeContext(),
+      false,
+      ''
+    );
+
+    $this->assertFalse($comment->hasSuggestion);
+    $this->assertEqual('', $comment->suggestionText);
+  }
+
+  public function testFileContextAndLinkStored() {
+    $comment = new EmailInlineComment(
+      '/src/bar.php:42',
+      'https://example.com/D2#inline-99',
+      $this->newMessage(),
+      'reply',
+      $this->newCodeContext()
+    );
+
+    $this->assertEqual('/src/bar.php:42', $comment->fileContext);
+    $this->assertEqual('https://example.com/D2#inline-99', $comment->link);
+    $this->assertEqual('reply', $comment->contextKind);
+  }
+}
